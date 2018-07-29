030beeebebf935b6733ccbf7176a76a29c8806aa
SatyaCts <satyapriya48@gmail.com>
2018-07-29 20:56:27 +0530

    Add files via upload

---
+++ SatyaDemo.java
@@ -0,0 +1,10 @@
+package main;
+
+public class SatyaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...This is satya's first demo java project");
+	}
+
+}
